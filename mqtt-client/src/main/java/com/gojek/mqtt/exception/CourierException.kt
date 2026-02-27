842e6f6c0e4bff292d4f5cbb0bcbe600e402263c
deepanshu <deepanshu@gojek.com>
2026-02-27 15:48:39 +0530

    Add exception class name to CourierException

--- mqtt-client/src/main/java/com/gojek/mqtt/exception/CourierException.kt
+++ mqtt-client/src/main/java/com/gojek/mqtt/exception/CourierException.kt
@@ -5,6 +5,7 @@ import org.eclipse.paho.client.mqttv3.MqttException
 
 data class CourierException(
     val reasonCode: Int = -1,
+    val type: String? = null,
     override val message: String? = null,
     override val cause: Throwable? = null
 ) : Throwable(message, cause)
@@ -14,18 +15,21 @@ internal fun Throwable?.toCourierException(): CourierException {
         CourierException()
     } else if (this is MqttException) {
         CourierException(
+            type = this::class.java.simpleName,
             reasonCode = reasonCode,
             message = cause?.message ?: message,
             cause = cause?.cause ?: cause
         )
     } else if (this is AuthApiException) {
         CourierException(
+            type = this::class.java.simpleName,
             reasonCode = reasonCode,
             message = message,
             cause = cause
         )
     } else {
         CourierException(
+            type = this::class.java.simpleName,
             message = message,
             cause = cause
         )
